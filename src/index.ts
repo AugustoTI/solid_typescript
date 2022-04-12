c72776888bf971878f821e7c43394d830930ffa6
AugustoTI <augustocesarti2021@gmail.com>
2022-04-12 01:24:25 -0300

    Observações sobre o S.O.L.I.D

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/*
+Vantagens e Desvantagens do S.O.L.I.D
+
+Vantagens:
+- Código modular
+- Código reutilizável (D.R.Y - Don't Repeat Yourself)
+- Código testável, baixo acoplamento
+- Baixo acoplamento e alta coesão
+- Código expansível
+- Separação de conceitos (Separation of concerns)
+- Fácil manutenção
+Desvantagens:
+- Complexidade
+- Quantidade de Código
+- Tempo de desenvolvimento aumenta
+- Tenha cuidado com: YAGNI(You aren't gonna need it), KISS (Keep it simple, stupid !)
+*/
